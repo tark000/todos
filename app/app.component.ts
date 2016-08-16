458461ed11b007a691b4baccea2c9fb58eed6939
taras <tark000@ukr.net>
2016-08-16 23:27:43 +0300

    0.1f

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,13 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'todo-app',
-    template: '<h1>Angular 2Do</h1>'
+    templateUrl: './app/app.components.html',
+    styleUrls: ['./app/app.components.css']
 })
-export class AppComponent { }
+export class AppComponent {
+	title: string;
+
+	constructor() {
+		this.title = 'Angular 2Do';
+	}
+}
